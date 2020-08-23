57043a130a21a5374d398236906a9ca8dd921d56
wikky2012 <wicliferono@gmail.com>
2020-08-23 19:30:33 +0300

    add quote functionality

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,12 +9,12 @@ import { Quote } from '../quote';
 export class QuoteComponent implements OnInit {
   
   quotes:Quote[]=[
-    new Quote(1,'The secret of getting ahead is getting started.','Provide description here'),
-    new Quote(2,'Only the paranoid survive.','Provide description here'),
-    new Quote(3,'Everything you can imagine is real.','Provide description here'),
-    new Quote(4,'Do what you feel in your heart to be right','Provide description here'),
-    new Quote(5,'Happiness is not something ready made. It comes from your own actions.','Provide description here'),
-    new Quote(6,'Whatever you are, be a good one..','Provide description here'),
+    new Quote(1,'The secret of getting ahead is getting started.','Provide description here', new Date(2019,9,14)),
+    new Quote(2,'Only the paranoid survive.','Provide description here', new Date(2019,9,14)),
+    new Quote(3,'Everything you can imagine is real.','Provide description here', new Date(2019,9,14)),
+    new Quote(4,'Do what you feel in your heart to be right','Provide description here', new Date(2019,9,14)),
+    new Quote(5,'Happiness is not something ready made. It comes from your own actions.','Provide description here', new Date(2019,9,14)),
+    new Quote(6,'Whatever you are, be a good one..','Provide description here', new Date(2019,9,14)),
   ]; 
 
   toggleDetails(index){
@@ -24,6 +24,13 @@ export class QuoteComponent implements OnInit {
     if (isComplete) {
       this.quotes.splice(index,1);
     }
+    
+  }
+  addNewQuote(quote){
+    let goalLength = this.quotes.length;
+    quote.id = goalLength+1;
+    quote.completeDate = new Date(quote.completeDate)
+    this.quotes.push(quote)
   }
   constructor() { }
 
